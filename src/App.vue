61eeb198cf0f9d38f080b1b6627a7c5a96d439a0
hc <1964316013@qq.com>
2022-06-25 16:57:13 +0800

    test: debug

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class=""></div>
+  <div class="">{{ obj }}</div>
 </template>
 
 // 来点注释，看看效果
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const obj = {
+  name: "why"
+};
+</script>
 
-<style scoped lang="less">
-
-
-
-</style>
+<style scoped lang="less"></style>
