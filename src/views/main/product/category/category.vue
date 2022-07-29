dd143d8e5bc94a479662c541e30f76c72658c579
HeCong <1964316013@qq.com>
2022-07-29 09:47:20 +0800

    feat(category): add category
    ..

--- src/views/main/product/category/category.vue
+++ src/views/main/product/category/category.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="category">
-    <h2>category</h2>
+    <page-search
+      :searchFormConfig="searchFormConfig"
+      @resetBtnClick="handleResetClick"
+      @queryBtnClick="handleQueryClick"
+    ></page-search>
+    <page-content
+      ref="pageContentRef"
+      :contentTableConfig="contentTableConfig"
+      pageName="category"
+      newBtnText="新建类别"
+      @newBtnClick="handleNewData"
+      @editBtnClick="handleEditData"
+    ></page-content>
+    <page-modal
+      ref="pageModalRef"
+      page-name="category"
+      :modal-config="modalConfig"
+      :defaultInfo="defaultInfo"
+    ></page-modal>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import PageSearch from "@/components/page-search";
+import PageContent from "@/components/page-content";
+import PageModal from "@/components/page-modal";
 
-export default defineComponent({
-  name: "category",
-  setup() {
-    return {};
-  }
-});
+import { searchFormConfig } from "./config/search.config";
+import { contentTableConfig } from "./config/content.config";
+import { modalConfig } from "./config/modal.config";
+
+import { usePageSearch } from "@/hooks/usePageSearch";
+import { usePageModal } from "@/hooks/usePageModal";
+
+const [pageContentRef, handleResetClick, handleQueryClick] = usePageSearch();
+
+const [defaultInfo, pageModalRef, handleNewData, handleEditData] =
+  usePageModal();
 </script>
 
 <style scoped></style>
